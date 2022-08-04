a782fcfdac70f2fe8ad97f1cb15779cba1a16b87
Vineeth Pulipati <mynameisvineeth@gmail.com>
2022-08-04 17:27:52 -0400

    Time: 15 ms (10.88%), Space: 15.5 MB (6.06%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string res ="";
+        for(int i=0;i<strs[0].size();++i){
+            for(auto s:strs){
+                if(i==s.size()||s[i]!=strs[0][i]){
+                    return res;
+                }
+            }
+            res+=strs[0][i];
+        }
+        return res;
+    }
+};
